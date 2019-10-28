10dc5f6eda4aa22ef566eaf01d4c0f93ed84ccaf
Yurika Jachmann <yj647@nyu.edu>
2019-10-27 22:15:22 -0400

    Adding registration and login forms

---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, validators, IntegerField, StringField, SubmitField
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', [validators.DataRequired()])
+    password = PasswordField('Password', [validators.DataRequired()])
+    two_factor = StringField('Phone Number', [validators.DataRequired()])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', [validators.DataRequired()])
+    password = PasswordField('Password', [validators.DataRequired()])
+    two_factor = StringField('Phone Number', [validators.DataRequired()])
+    submit = SubmitField('Sign In')
+
+class SpellCheckForm(FlaskForm):
+    inputtext = TextAreaField('Input Text')
