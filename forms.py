0e4d5d300a85d905c55575f85622a239017a9fe7
Yurika Jachmann <yj647@nyu.edu>
2019-10-27 23:07:12 -0400

    Updating 2ofa id

--- forms.py
+++ forms.py
@@ -4,13 +4,13 @@ from wtforms import PasswordField, validators, IntegerField, StringField, Submit
 class RegistrationForm(FlaskForm):
     uname = StringField('Username', [validators.DataRequired()])
     pword = PasswordField('Password', [validators.DataRequired()])
-    mfa = StringField('Phone Number', [validators.DataRequired()])
+    mfa = StringField('Phone Number', id='2ofa', [validators.DataRequired()])
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
     uname = StringField('Username', [validators.DataRequired()])
     pword = PasswordField('Password', [validators.DataRequired()])
-    mfa = StringField('Phone Number', [validators.DataRequired()])
+    mfa = StringField('2ofa', id='2ofa', [validators.DataRequired()])
     submit = SubmitField('Sign In')
 
 class SpellCheckForm(FlaskForm):
