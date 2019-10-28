51862bc227ab8c59642a66231d0a15b0007ac801
Yurika Jachmann <yj647@nyu.edu>
2019-10-27 23:20:03 -0400

    2fa no longer required

--- forms.py
+++ forms.py
@@ -4,13 +4,13 @@ from wtforms import PasswordField, validators, IntegerField, StringField, Submit
 class RegistrationForm(FlaskForm):
     uname = StringField('Username', [validators.DataRequired()])
     pword = PasswordField('Password', [validators.DataRequired()])
-    mfa = PasswordField('2ofa', id='2ofa', validators= [validators.DataRequired()])
+    mfa = PasswordField('2ofa', id='2ofa')
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
     uname = StringField('Username', [validators.DataRequired()])
     pword = PasswordField('Password', [validators.DataRequired()])
-    mfa = PasswordField('2ofa', id='2ofa', validators = [validators.DataRequired()])
+    mfa = PasswordField('2ofa', id='2ofa')
     submit = SubmitField('Sign In')
 
 class SpellCheckForm(FlaskForm):
